10aadfdf28bd78895f73e0b0f0919723cdb1bf7d
draro <58738792+draro@users.noreply.github.com>
2020-02-05 09:24:05 -0500

    Updates

---
+++ precache-manifest.9165af207efd3f06db9e43a34255f209.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6966602b39699db9634b87ccbda9718b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab745e608640f0212972",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "13ee5d2ad24073f57f80",
+    "url": "/static/css/main.da7edea2.chunk.css"
+  },
+  {
+    "revision": "ab745e608640f0212972",
+    "url": "/static/js/2.7c5e67a5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.7c5e67a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13ee5d2ad24073f57f80",
+    "url": "/static/js/main.645b2b81.chunk.js"
+  },
+  {
+    "revision": "b20a1b5a49a61d47deef",
+    "url": "/static/js/runtime-main.eadeb9cb.js"
+  },
+  {
+    "revision": "1330fb23cfebc286c41b584d9662630a",
+    "url": "/static/media/Logo.1330fb23.png"
+  }
+]);
