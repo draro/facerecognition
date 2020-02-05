ced9e1825c7777f98122bee517296617efb2ac7a
draro <58738792+draro@users.noreply.github.com>
2020-02-05 08:04:48 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,15 +6,12 @@ import Rank from './components/Rank/Rank';
 import FaceRecgnition from './components/FaceRecognition/FaceRecognition';
 import Signin from './components/Signin/Signin';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
+
 import './App.css';
 import { Component } from 'react';
 import 'tachyons';
 import Register from './components/Register/Register';
 
-const app = new Clarifai.App({
-  apiKey: '0686238167414d29915e8189aff4fc83'
-});
 
 const particlesOptions = {
   particles: {
@@ -80,8 +77,14 @@ class App extends Component {
   }
   onSubmit = () => {
     this.setState({ imageUrl: this.state.input })
-    app.models.predict(Clarifai.FACE_DETECT_MODEL,
-      this.state.input)
+    fetch('http://localhost:3001/imageUrl', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+      .then(response => response.json())
       .then(response => {
         if (response) {
           fetch('http://localhost:3001/image', {
